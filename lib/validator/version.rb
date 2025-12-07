7e44e4030f518ba61a0cdeeca3406cc2e133b7be
Daniel Nottingham <danielnottingham.ti@gmail.com>
2025-12-07 11:28:00 -0300

    Initial commit

---
+++ lib/validator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Validator
+  VERSION = "0.1.0"
+end
---
+++ spec/validator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Validator do
+  it "has a version number" do
+    expect(Validator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
